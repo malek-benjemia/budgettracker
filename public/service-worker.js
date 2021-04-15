bbf1424020437be6ae55791e2facd3468979f3ff
MalekBenJemia <malekbenjemia@gmail.com>
2021-04-14 22:50:20 -0400

    all works except the offline localhost does not load index

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,13 +3,16 @@ const VERSION = 'version_01';
 const CACHE_NAME = APP_PREFIX + VERSION
 const FILES_TO_CACHE = [
   "./index.html",
+  "./manifest.json",
   "./css/styles.css",
-  "./js/index.js"
+  "./js/index.js",
+  "./js/idb.js"
 ];
 
 // Respond with cached resources
 self.addEventListener('fetch', function (e) {
   console.log('fetch request : ' + e.request.url)
+  console.log(caches);
   e.respondWith(
     caches.match(e.request).then(function (request) {
       if (request) { // if cache is available, respond with cache
@@ -35,6 +38,7 @@ self.addEventListener('install', function (e) {
     })
   )
 })
+
 
 // Delete outdated caches
 self.addEventListener('activate', function (e) {
